26d82f555b21625af8be099ecfd4eb03fcf49ed9
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-18 11:58:44 +0200

    fájl beolvasás

--- cukraszda/Form1.cs
+++ cukraszda/Form1.cs
@@ -13,6 +13,7 @@ namespace cukraszda
 {
     public partial class Form1 : Form
     {
+        List<Sutemeny> sutemenies = new List<Sutemeny>();
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +21,19 @@ namespace cukraszda
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-
+            using (StreamReader sr = new StreamReader("cuki.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] tmp = sr.ReadLine().Split(';');
+                    string nev = tmp[0];
+                    string tipus = tmp[1];
+                    bool dijazott = Convert.ToBoolean(tmp[2]);
+                    int ar = Convert.ToInt32(tmp[3]);
+                    string egyseg = tmp[4];
+                    sutemenies.Add(new Sutemeny(nev, tipus, dijazott, ar, egyseg));
+                }
+            }
         }
     }
 }
